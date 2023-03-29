2737bf582f94a3ca2a328c3a4c4987b83e43724c
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-03-29 12:08:47 -0300

    lab040506

--- LAB-06/provider.tf
+++ LAB-06/provider.tf
@@ -1,14 +1,7 @@
 terraform {
-  required_version = ">=0.12"
-
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>2.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-  }
+  required_version = ">=2.99.0"
+}
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
 }
