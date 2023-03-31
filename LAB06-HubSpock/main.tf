d0b61fde4199a0b7af16fd0c21b29b63667ab93e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-03-31 16:40:04 -0300

    lab040506

--- LAB06-HubSpock/main.tf
+++ LAB06-HubSpock/main.tf
@@ -141,16 +141,12 @@ resource "azurerm_network_interface" "vm03" {
 
 
 #peringconection
-resource "azurerm_virtual_network_peering" "pering1" {
-  name                         = "vnet1tovnet2"
+resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
+  name                         = "vnet1-to-vnet2"
   resource_group_name          = azurerm_resource_group.az104-06.name
   virtual_network_name         = azurerm_virtual_network.vnet1.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet2.name
-}
-
-resource "azurerm_virtual_network_peering" "pering2" {
-  name                         = "vnet1tovnet2"
-  resource_group_name          = azurerm_resource_group.az104-06.name
-  virtual_network_name         = azurerm_virtual_network.vnet2.name
-  remote_virtual_network_id    = azurerm_virtual_network.vnet3.name
+  remote_virtual_network_id    = azurerm_virtual_network.vnet2.id
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
 }
