a7375f3a7fdf6b9193c0558563d08230ccc89779
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-03-31 17:07:22 -0300

    lab040506

--- LAB06-HubSpock/main.tf
+++ LAB06-HubSpock/main.tf
@@ -169,20 +169,20 @@ resource "azurerm_route_table" "az10406" {
   resource_group_name = azurerm_resource_group.az104-06.name
 
   route {
-    name           = "az104-06-route1-vnet1-to-vnet3"
+    name           = "az104-06-route-vnet0-to-vnet3"
     address_prefix = "10.63.0.0/24"
     next_hop_type  = "VirtualAppliance"
   }
 
   route {
-    name           = "az104-06-route2-vnet1-to-vnet3"
-    address_prefix = "10.60.1.0/24"
+    name           = "az104-06-route-vnet0-to-vnet3"
+    address_prefix = "10.60.0.0/24"
     next_hop_type  = "VirtualAppliance"
-    next_hop_in_ip_address = "10.60.1.4"
+    next_hop_in_ip_address = "10.60.0.4"
   }
 }
 
 resource "azurerm_subnet_route_table_association" "routeaz104" {
-  subnet_id      = azurerm_subnet.subnet1.id
+  subnet_id      = azurerm_subnet.subnet0.id
   route_table_id = azurerm_route_table.az10406.id
 }
