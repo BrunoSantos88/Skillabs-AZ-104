1edb75bff4e959c3ee2abbc1fc3d90eaddffe183
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-03-31 16:42:31 -0300

    lab040506

--- LAB06-HubSpock/main.tf
+++ LAB06-HubSpock/main.tf
@@ -150,3 +150,13 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet2" {
   allow_gateway_transit        = false
   use_remote_gateways          = false
 }
+
+resource "azurerm_virtual_network_peering" "vnet1_to_vnet3" {
+  name                         = "vnet1-to-vnet3"
+  resource_group_name          = azurerm_resource_group.az104-06.name
+  virtual_network_name         = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id    = azurerm_virtual_network.vnet3.id
+  allow_forwarded_traffic      = true
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+}
