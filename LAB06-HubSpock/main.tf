aa75868c69aeab4814d1aaa3154fcaaadf3b9d0e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-01 20:49:00 -0300

    hubspock

--- LAB06-HubSpock/main.tf
+++ LAB06-HubSpock/main.tf
@@ -160,3 +160,29 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet3" {
   allow_gateway_transit        = false
   use_remote_gateways          = false
 }
+
+#routetable
+
+resource "azurerm_route_table" "rt23" {
+  name                = "az104-06-rt23"
+  location            = "eastus"
+  resource_group_name = azurerm_resource_group.az104-06.name
+
+  route {
+    name           = "rota1"
+    address_prefix = "10.62.0.0/24"
+    next_hop_type  = "VirtualAppliance"
+  }
+
+  route {
+    name           = "rota2"
+    address_prefix = "10.60.0.0/24"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = "10.60.0.4"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "rt23" {
+  subnet_id      = azurerm_subnet.subnet0.name
+  route_table_id = azurerm_route_table.rt23.id
+}
