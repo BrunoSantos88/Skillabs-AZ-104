aefc6b0722645292a2fa3070c5307ab55f0c5c33
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 18:59:20 -0300

    hubspok

--- LAB06-HubSpock/mysql.tf
+++ LAB06-HubSpock/mysql.tf
@@ -1,35 +1,3 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "my-virtual-network"
-  address_space       = ["10.63.0.0/22"]
-  location            = azurerm_resource_group.az104-06.location
-  resource_group_name = azurerm_resource_group.az104-06.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
-  resource_group_name = azurerm_resource_group.az104-06.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.63.0.0/24"]
-  service_endpoints    = ["Microsoft.Sql"]
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "my-network-interface"
-  location            = azurerm_resource_group.az104-06.location
-  resource_group_name = azurerm_resource_group.az104-06.name
-
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.63.0.4"
-    }
-
-    tags = {
-    environment = "BancoDados"
-    }
-}
-
 resource "azurerm_mysql_server" "example" {
   name                = "az104mysqlserver"
   location            = azurerm_resource_group.az104-06.location
@@ -51,9 +19,27 @@ resource "azurerm_mysql_virtual_network_rule" "example" {
   name                = "mysql-vnet-rule"
   resource_group_name = azurerm_resource_group.az104-06.name
   server_name         = azurerm_mysql_server.example.name
-  subnet_id           = azurerm_subnet.example.id
+  subnet_id           = azurerm_subnet.subnet3.id
 }
 
+
+#interface Banco de Dados
+resource "azurerm_network_interface" "vnet-mysql" {
+  name                = "vm-mysqlserver"
+  location            = azurerm_resource_group.az104-06.location
+  resource_group_name = azurerm_resource_group.az104-06.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet3.id
+    private_ip_address_allocation = "Static"
+    private_ip_address            = "10.63.0.4"
+    }
+
+    tags = {
+    environment = "BancoDados"
+    }
+}
 
 
 
