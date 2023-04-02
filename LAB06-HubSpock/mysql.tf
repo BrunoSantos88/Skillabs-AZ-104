0c900a76d6867b8d8f35585c5c5714c05b13808e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 18:18:25 -0300

    hubspok

--- LAB06-HubSpock/mysql.tf
+++ LAB06-HubSpock/mysql.tf
@@ -1,7 +1,33 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "my-virtual-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.az104-06.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "my-subnet"
+  resource_group_name = azurerm_resource_group.az104-06.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "my-network-interface"
+  location            = "westus2"
+  resource_group_name = azurerm_resource_group.az104-06.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_mysql_server" "example" {
   name                = "my-mysql-server"
   resource_group_name = azurerm_resource_group.az104-06.name
-  location            = azurerm_resource_group.az104-06.location
+  location            = "westus2"
   sku_name            = "B_Gen5_1"
   storage_mb          = 5120
   administrator_login = "mysqladmin"
@@ -9,29 +35,6 @@ resource "azurerm_mysql_server" "example" {
   version = "5.7"
 
   private_network_interface_ids = [
-    azurerm_network_interface.vm03.id,
+    azurerm_network_interface.example.id,
   ]
-
-  ssl_enforcement_enabled = true
-  ssl_minimal_tls_version_enforced = "TLSEnforcement_1_2"
-
-  fqdn = "my-mysql-server.internal"
-}
-###regras de rede
-resource "azurerm_network_interface" "vm03" {
-  name                = "vm3nic"
-  location            = azurerm_resource_group.az104-06.location
-  resource_group_name = azurerm_resource_group.az104-06.name
-  enable_ip_forwarding = true
-
-    ip_configuration {
-    name                          = "vm3nic"
-    subnet_id                     = azurerm_subnet.subnet3.id
-    private_ip_address_allocation = "Static"
-    private_ip_address            = "10.63.0.4"
-    }
-
-    tags = {
-    environment = "az106-06-vm3"
-    }
 }
