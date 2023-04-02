1bf3db58ed6f8ae8b180fb42e055a33cc8b558f6
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 18:11:23 -0300

    hubspok

--- LAB06-HubSpock/mysql.tf
+++ LAB06-HubSpock/mysql.tf
@@ -1,30 +1,22 @@
-resource "azurerm_mysql_server" "az104myzsql" {
-  name                = "myzsqlserver"
-  location            = azurerm_resource_group.az104-06.location
-  resource_group_name = azurerm_resource_group.az104-06.name
-  administrator_login = "AdmimAdmym88"
-  administrator_login_password = "*Mulekao1988*"
-  sku_name   = "GP_Gen5_2"
-  storage_mb = 5120
-  version    = "5.7"
+resource "azurerm_mysql_server" "example" {
+  name                = "my-mysql-server"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = "westus2"
+  sku_name            = "B_Gen5_1"
+  storage_mb          = 5120
+  administrator_login = "mysqladmin"
+  administrator_login_password = "MyP@ssw0rd!"
+  version = "5.7"
 
-  auto_grow_enabled                 = true
-  backup_retention_days             = 7
-  geo_redundant_backup_enabled      = true
-  infrastructure_encryption_enabled = true
-  public_network_access_enabled     = false
-  ssl_enforcement_enabled           = true
-  ssl_minimal_tls_version_enforced  = "TLS1_2"
+  private_network_interface_ids = [
+    azurerm_network_interface.vm03.id,
+  ]
+
+  ssl_enforcement_enabled = true
+  ssl_minimal_tls_version_enforced = "TLSEnforcement_1_2"
+
+  fqdn = "my-mysql-server.internal"
 }
-
-resource "azurerm_mysql_database" "az104mywsql" {
-  name                = "mywsqlserver"
-  resource_group_name = azurerm_resource_group.az104-06.name
-  server_name         = azurerm_mysql_server.az104myzsql.name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
-}
-
 ###regras de rede
 resource "azurerm_network_interface" "vm03" {
   name                = "vm3nic"
@@ -42,11 +34,4 @@ resource "azurerm_network_interface" "vm03" {
     tags = {
     environment = "az106-06-vm3"
     }
-}
-
-resource "azurerm_mysql_virtual_network_rule" "example" {
-  name                = "mysql-vnet-rule"
-  resource_group_name = azurerm_resource_group.az104-06.name
-  server_name         = azurerm_mysql_server.az104myzsql.name
-  subnet_id           = azurerm_subnet.subnet3.id
 }
