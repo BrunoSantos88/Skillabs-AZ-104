99b6d34286b72dde98a66d52c08b3fe78dd7c4cb
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 13:44:55 -0300

    hubspok

--- LAB06-HubSpock/myql.tf
+++ LAB06-HubSpock/myql.tf
@@ -1,7 +1,26 @@
+resource "azurerm_resource_group" "example" {
+  name     = "myresourcegroup"
+  location = "eastus"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "myvnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "mysubnet"
+  address_prefixes     = ["10.0.1.0/24"]
+  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.example.name
+}
+
 resource "azurerm_mysql_server" "example" {
   name                = "mydatabase"
-  location            = azurerm_resource_group.az104-06.location
-  resource_group_name = azurerm_resource_group.az104-06.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
   administrator_login          = "myadminuser"
   administrator_login_password = "myadminpassword"
 
@@ -10,5 +29,13 @@ resource "azurerm_mysql_server" "example" {
 
   ssl_enforcement_enabled = true
 
-  subnet_id = azurerm_subnet.subnet3.id
+  private_endpoint_connections {
+    name                          = "myprivateendpoint"
+    private_connection_resource_id = azurerm_mysql_server.example.id
+    is_manual_connection          = false
+    private_connection_subnet_id  = azurerm_subnet.example.id
+    private_ip_address            = "10.0.1.5" # Static IP address for MySQL server in the subnet
+  }
+
+  subnet_id = azurerm_subnet.example.id
 }
