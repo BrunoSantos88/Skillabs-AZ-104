cf85d1586e0db8d765cb77797bf3e36fef26ac96
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 15:43:27 -0300

    hubspok

--- LAB06-HubSpock/myql.tf
+++ LAB06-HubSpock/myql.tf
@@ -1,16 +1,23 @@
-resource "azurerm_mssql_server" "example" {
-  name                         = "uniqueazuresqlserver"
-  resource_group_name          = azurerm_resource_group.az104-06.name
-  location                     = azurerm_resource_group.az104-06.location
-  version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+resource "azurerm_mysql_server" "example" {
+  name                = "example-mysqlserver"
+  location            = azurerm_resource_group.az104-06.location
+  resource_group_name = azurerm_resource_group.az104-06.name
+
+  administrator_login          = "mysqladminun"
+  administrator_login_password = "H@Sh1CoR3!"
+
+  sku_name   = "GP_Gen5_2"
+  storage_mb = 5120
+  version    = "5.7"
+
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  ssl_enforcement_enabled      = true
 }
 
-resource "azurerm_mssql_virtual_network_rule" "example" {
-  name      = "sql-vnet-rule"
-  server_id = azurerm_mssql_server.example.id
-  subnet_id = azurerm_subnet.subnet3.id
-  private_ip_address_allocation = "Static"
-  private_ip_address            = "10.63.0.4"
+resource "azurerm_mysql_virtual_network_rule" "example" {
+  name                = "mysql-vnet-rule"
+  resource_group_name = azurerm_resource_group.az104-06.name
+  server_name         = azurerm_mysql_server.example.name
+  subnet_id           = azurerm_subnet.subnet3.id
 }
