6a6b4d453ccfe54a29422cbc8653e032baba497a
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 16:19:44 -0300

    hubspok

--- LAB06-HubSpock/server.sql.tf
+++ LAB06-HubSpock/server.sql.tf
@@ -2,8 +2,8 @@ resource "azurerm_mysql_server" "az104-06sql" {
   name                = "mysql-server"
   location            = azurerm_resource_group.az104-06.location
   resource_group_name = azurerm_resource_group.az104-06.name
-  administrator_login = "exampleadmin"
-  administrator_login_password = "examplepassword"
+  administrator_login = "admin"
+  administrator_login_password = "*mulekao1988*"
   sku_name   = "GP_Gen5_2"
   storage_mb = 5120
   version    = "5.7"
