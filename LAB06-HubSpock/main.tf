2d84cf65f7cde362a40ab2ab2e1b57587c0e4aef
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 12:26:24 -0300

    hubspok

--- LAB06-HubSpock/main.tf
+++ LAB06-HubSpock/main.tf
@@ -221,3 +221,33 @@ resource "azurerm_subnet_route_table_association" "rt32" {
   subnet_id      = azurerm_subnet.subnet1.id
   route_table_id = azurerm_route_table.rt32.id
 }
+
+resource "azurerm_route_table" "rt42" {
+  name                = "az-10406-rt42"
+  location            = "East US"
+  resource_group_name = azurerm_resource_group.az104-06.name
+}
+
+
+  resource "azurerm_route" "rt32-route1" {
+  name                = "route-1"
+  address_prefix      = "10.63.0.0/24"
+  next_hop_type       = "VirtualAppliance"
+  next_hop_in_ip_address = "10.63.0.4"
+  route_table_name    = azurerm_route_table.rt32.name
+  resource_group_name = azurerm_resource_group.az104-06.name
+}
+
+resource "azurerm_route" rt32-route2 {
+  name                = "route-2"
+  address_prefix      = "10.62.0.0/24"
+  next_hop_type       = "VirtualAppliance"
+  next_hop_in_ip_address = "10.60.0.4"
+  route_table_name    = azurerm_route_table.rt42.name
+  resource_group_name = azurerm_resource_group.az104-06.name
+}
+
+resource "azurerm_subnet_route_table_association" "rt42" {
+  subnet_id      = azurerm_subnet.subnet2.id
+  route_table_id = azurerm_route_table.rt42.id
+}
