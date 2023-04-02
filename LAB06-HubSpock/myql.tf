1e3f6e38860efeecbcc206cc243106492ebba44f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 14:56:52 -0300

    hubspok

--- LAB06-HubSpock/myql.tf
+++ LAB06-HubSpock/myql.tf
@@ -1,25 +1,14 @@
-resource "azurerm_mysql_server" "example" {
-  name                = "example-mysql-server"
-  location            = "East US"
-  resource_group_name = "example-resource-group"
-  sku_name            = "B_Gen5_1"
-  storage_mb          = 5120
-  version             = "5.7"
-
-  administrator_login          = "mysqladmin"
-  administrator_login_password = "password123"
-
-  # Associate the server with the example-subnet
-  subnet_id = azurerm_subnet.subnet3.id
-
+resource "azurerm_mssql_server" "example" {
+  name                         = "uniqueazuresqlserver"
+  resource_group_name          = azurerm_resource_group.az104-06.name
+  location                     = azurerm_resource_group.az104-06.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
-resource "azurerm_mysql_database" "example" {
-  name                = "example-mysql-database"
-  resource_group_name = azurerm_resource_group.az104-06.name
-  server_name         = azurerm_mysql_server.example.name
-  charset             = "utf8"
-  collation           = "utf8_general_ci"
-
-  # ...
+resource "azurerm_mssql_virtual_network_rule" "example" {
+  name      = "sql-vnet-rule"
+  server_id = azurerm_mssql_server.example.id
+  subnet_id = azurerm_subnet.subnet3.id
 }
