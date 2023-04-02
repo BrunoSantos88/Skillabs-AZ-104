32ffa374e67be4fb82581866afa66dd010d8eb6f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-01 22:35:49 -0300

    hubspock

--- LAB06-HubSpock/keyvault.tf
+++ LAB06-HubSpock/keyvault.tf
@@ -1,7 +1,15 @@
+resource "azurerm_key_vault" "mykeyvault" {
+  name                        = "my-key-vault"
+  location           = azurerm_resource_group.az104-06.location
+  resource_group_name = azurerm_resource_group.az104-06.name
+  enabled_for_disk_encryption = true
+
+}
+
 resource "azurerm_key_vault_secret" "az104-key" {
   name         = "my-ssh-private-key"
   value        = file("~/.ssh/id_rsa")
   content_type = "text/plain"
 
-  key_vault_id = azurerm_key_vault.example.id
+  key_vault_id = azurerm_key_vault.mykeyvault.id
 }
