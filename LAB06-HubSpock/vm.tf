37737111a78a2bb3acab9b152c860182f0e1e4e3
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-01 22:58:11 -0300

    hubspok

--- LAB06-HubSpock/vm.tf
+++ LAB06-HubSpock/vm.tf
@@ -20,18 +20,13 @@ resource "azurerm_linux_virtual_machine" "az104-vm0" {
   }
 
   os_profile {
-    computer_name  = "my-vm"
+    computer_name  = "az104-vm0"
     admin_username = "adminuser"
     admin_password = "adminpassword"
   }
 
   os_profile_linux_config {
-    custom_data = base64encode("#!/bin/bash\necho 'Hello, World!' > /tmp/hello-world.txt")
-
-    ssh_key {
-      key_data      = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHiBXSqnB0ApgyG2M2n6SxJd9alb8MTKVI/dd834eQV/rdMlw2nXEvoreMqBQFc1K09xbzWWldDLBAROHhRxbO4hACt8X1OPRkswyLHH+im6ee9/k3CIxvOO/PqQ9JLDx/ig4WgLud8QnxT8nu893yl3lJMO+XhXh+7pbaL69zsLi1ZfxGo6pbGhZV6xTzILk/WParqkzDc3/s5FsGYej7oaacs65zm2hugatbASJJpwOBypcIUpyehdACVu8VAHWMIaGYIuplFS1v2a3Vg0INCyJS/X8BG768ov7Q43x/tqUM3weaRlilPWAL3u3XZsRgRXOmOfUvZQoKO1ZJfLLuG/HpUzDMWKPcp1YqV3JChbZVQACx/AeJxGyUeGepv2EbvgIwu2B3ONntW7qXVK+ck1Kx8ZQUhotM/FcHSNPmxrToQKYA9Ytu4CsD5pFPlOAIaYzXgRbfg7ZZNuuKir/teUB+4fZqH9p6KaYg1fkGLfE5F+u4k+rmTEd90XQ5qc7N7LwCcAGJtg1Zd+QBVqcXTyJmyUDJMbgwMI/RZIO46cNJbwiNE2F+JJvAEz+InyQa7RR+z684jq8P54RbszGmMotzprECvc1/aLC1zB6Gf/JrjFotyWo/qLAL0Ue2u+1IO91F+3iWgNaLZ43KPe/djcjAqAteoZzScRK+jIxlCw== brunosantosc1@gmail.com"
-      path          = "/home/myadminusername/.ssh/authorized_keys"
-    }
+    disable_password_authentication = false
   }
 
   tags = {
