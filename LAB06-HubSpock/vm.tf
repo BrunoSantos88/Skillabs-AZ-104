a2fd3441cb082ecf27ef64f41ae9bbbdeade3276
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-01 22:07:34 -0300

    hubspock

--- LAB06-HubSpock/vm.tf
+++ LAB06-HubSpock/vm.tf
@@ -1,36 +1,24 @@
-data "azurerm_image" "ubuntu" {
-  name                = "UbuntuServer"
-  resource_group_name = "Canonical"
-  publisher           = "Canonical"
-  offer               = "UbuntuServer"
-  sku                 = "20.04-LTS"
-}
-
-resource "azurerm_virtual_machine" "az104-06-v0" {
-  name                  = "az104-06-vm0"
-  location              = "East US"
-  resource_group_name   = azurerm_resource_group.az104-06.name
+resource "azurerm_linux_virtual_machine" "az104-vm0" {
+  name                  = "az-104-06-vm0"
+  location            = azurerm_resource_group.az104-06.location
+  resource_group_name = azurerm_resource_group.az104-06.name
   network_interface_ids = [azurerm_network_interface.vm00.id]
-  vm_size               = "Standard_B2s"
 
-  storage_os_disk {
-    name              = "example-os-disk"
+  size                 = "Standard_B2s"
+  admin_username       = "myadminusername"
+  admin_password       = "myadminpassword"
+  disable_password_auth = false
+
+  os_disk {
+    name              = "myosdisk"
     caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
+    storage_account_type = "Standard_LRS"
   }
 
-  storage_image_reference {
-    id = data.azurerm_image.ubuntu.id
-  }
-
-  os_profile {
-    computer_name  = "example-vm"
-    admin_username = "adminuser"
-    admin_password = "Password1234!"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = false
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 }
