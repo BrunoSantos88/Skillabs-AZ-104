1bdd3854b8d93a22f00e36f33791a9b0fa66fb1b
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 16:37:08 -0300

    hubspok

--- LAB06-HubSpock/server.sql.tf
+++ LAB06-HubSpock/server.sql.tf
@@ -1,9 +1,9 @@
-resource "azurerm_mysql_server" "az104-06sql" {
+resource "azurerm_mysql_server" "az104mysql" {
   name                = "myysqlserver"
   location            = azurerm_resource_group.az104-06.location
   resource_group_name = azurerm_resource_group.az104-06.name
-  administrator_login = "adminadmin"
-  administrator_login_password = "*mulekao1988*"
+  administrator_login = "admynadmin"
+  administrator_login_password = "az104myqlsadmin"
   sku_name   = "GP_Gen5_2"
   storage_mb = 5120
   version    = "5.7"
@@ -17,22 +17,22 @@ resource "azurerm_mysql_server" "az104-06sql" {
   ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
-resource "azurerm_mysql_database" "example" {
+resource "azurerm_mysql_database" "az104mysql" {
   name                = "myysqlserver"
   resource_group_name = azurerm_resource_group.az104-06.name
-  server_name         = azurerm_mysql_server.az104-06sql.id
+  server_name         = azurerm_mysql_server.az104mysql.id
   charset             = "utf8"
   collation           = "utf8_unicode_ci"
 }
 
 resource "azurerm_mysql_virtual_network_rule" "az104-06netsql" {
   name                = "myysqlrules"
-  server_name         = azurerm_mysql_server.az104-06sql.name
+  server_name         = azurerm_mysql_server.az104mysql.name
   resource_group_name = azurerm_resource_group.az104-06.name
   subnet_id           = azurerm_subnet.subnet3.id
 
   depends_on = [
-    azurerm_mysql_server.az104-06sql,
+    azurerm_mysql_server.az104mysql,
     azurerm_network_interface.vm03
   ]
 }
