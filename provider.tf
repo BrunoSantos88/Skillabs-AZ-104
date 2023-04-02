29eddc3a38d10a555f3a63104cf9556d6b0263d3
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 13:45:42 -0300

    hubspok

--- provider.tf
+++ provider.tf
@@ -1,15 +1,3 @@
-# TERRAFORM PRE INSTALADO NO POWERSHELL BASH SO FUNCIONA 2.99.0
-
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=2.99.0"
-    }
-  }
-}
-
-
 provider "azurerm" {
   features {}
 }
