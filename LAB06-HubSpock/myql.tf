e20ac8f8948a8a76db16c0eac875253fe4af81c1
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 13:39:17 -0300

    hubspok

--- LAB06-HubSpock/myql.tf
+++ LAB06-HubSpock/myql.tf
@@ -1,27 +1,20 @@
 resource "azurerm_mysql_server" "example" {
-  name                = "example-mysql-server"
+  name                = "mydatabase"
   location            = azurerm_resource_group.az104-06.location
   resource_group_name = azurerm_resource_group.az104-06.name
-  sku_name            = "B_Gen5_1"
-  storage_mb          = 5120
-  version             = "5.7"
+  administrator_login          = "myadminuser"
+  administrator_login_password = "myadminpassword"
 
-  administrator_login          = "mysqladmin"
-  administrator_login_password = "password123"
+  sku_name   = "GP_Gen5_2"
+  storage_mb = 5120
 
-  network_configuration {
-    subnet_id = azurerm_virtual_network.subnet3.id
-    public_ip_address_id = null
-    private_ip_address_allocation = "Static"
+  ssl_enforcement_enabled = true
+
+  private_endpoint_connections {
+    name                          = "myprivateendpoint"
+    private_connection_resource_id = azurerm_mysql_server.example.id
+    is_manual_connection          = false
+    private_connection_subnet_id  = azurerm_subnet.subnet3.id
     private_ip_address            = "10.63.0.4"
-
-  }
 }
-
-resource "azurerm_mysql_database" "example" {
-  name                = "example-mysql-database"
-  server_name         = azurerm_mysql_server.example.name
-  resource_group_name = azurerm_resource_group.az104-06.name
-  charset             = "utf8"
-  collation           = "utf8_general_ci"
 }
