d8da9ce9bcf27f0453fbd5f3cfda89d795a4d0bb
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-01 23:11:51 -0300

    hubspok

--- LAB06-HubSpock/vm.tf
+++ LAB06-HubSpock/vm.tf
@@ -1,31 +1,35 @@
-resource "azurerm_linux_virtual_machine" "az104-vm0" {
-  name                = "az104-06-vm0"
+resource "azurerm_virtual_machine" "myvm" {
+  name                  = "my-vm"
   location           = azurerm_resource_group.az104-06.location
   resource_group_name = azurerm_resource_group.az104-06.name
-  size                 = "Standard_DS1_v2"
   network_interface_ids = [azurerm_network_interface.vm00.id]
+  vm_size               = "Standard_B1ls"
 
   storage_image_reference {
     publisher = "Canonical"
     offer     = "UbuntuServer"
-    sku       = "20.04-LTS"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
-   storage_os_disk {
+  storage_os_disk {
     name              = "my-os-disk"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
   }
 
-   os_profile {
+  os_profile {
     computer_name  = "my-vm"
     admin_username = "adminuser"
     admin_password = "adminpassword"
   }
 
-  tags = {
-    environment = "frontend"
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+
+  boot_diagnostics {
+    storage_account_uri = "https://mystorageaccount.blob.core.windows.net/"
   }
 }
