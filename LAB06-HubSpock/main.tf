8a7b25c0460e1389bfd6e93f71c802e05c25d642
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-01 21:02:07 -0300

    hubspock

--- LAB06-HubSpock/main.tf
+++ LAB06-HubSpock/main.tf
@@ -164,25 +164,29 @@ resource "azurerm_virtual_network_peering" "vnet1_to_vnet3" {
 #routetable
 
 resource "azurerm_route_table" "rt23" {
-  name                = "az104-06-rt23"
-  location            = "eastus"
+  name                = "az-10406-rt23"
+  location            = "East US"
   resource_group_name = azurerm_resource_group.az104-06.name
-
-  route {
-    name           = "rota1"
-    address_prefix = "10.62.0.0/24"
-    next_hop_type  = "VirtualAppliance"
-  }
-
-  route {
-    name           = "rota2"
-    address_prefix = "10.60.0.0/24"
-    next_hop_type  = "VirtualAppliance"
-    next_hop_in_ip_address = "10.60.0.4"
-  }
 }
 
-resource "azurerm_subnet_route_table_association" "rt23" {
+
+  resource "azurerm_route" "route1" {
+  name                = "route-1"
+  address_prefix      = "10.62.0.0/24"
+  next_hop_type       = "VirtualAppliance"
+  next_hop_in_ip_address = "10.62.0.4"
+  route_table_name    = azurerm_route_table.rt23.name
+}
+
+resource "azurerm_route" "route2" {
+  name                = "route-2"
+  address_prefix      = "10.60.0.0/24"
+  next_hop_type       = "VirtualAppliance"
+  next_hop_in_ip_address = "10.60.0.4"
+  route_table_name    = azurerm_route_table.rt23.name
+}
+
+resource "azurerm_subnet_route_table_association" "example" {
   subnet_id      = azurerm_subnet.subnet0.id
   route_table_id = azurerm_route_table.rt23.id
 }
