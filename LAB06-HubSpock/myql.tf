e8397b5707eb033da6b60f2e260c38b853cc8f4d
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 14:16:12 -0300

    hubspok

--- LAB06-HubSpock/myql.tf
+++ LAB06-HubSpock/myql.tf
@@ -1,7 +1,19 @@
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  location            = "East US"
+  resource_group_name =  azurerm_resource_group.az104-06.name
+  address_space       = ["10.0.0.0/16"]
+
+  subnet {
+    name           = "example-subnet"
+    address_prefix = "10.0.1.0/24"
+  }
+}
+
 resource "azurerm_mysql_server" "example" {
   name                = "example-mysql-server"
-  location            = azurerm_resource_group.az104-06.location
-  resource_group_name = azurerm_resource_group.az104-06.name
+  location            = "East US"
+  resource_group_name = "example-resource-group"
   sku_name            = "B_Gen5_1"
   storage_mb          = 5120
   version             = "5.7"
@@ -9,16 +21,6 @@ resource "azurerm_mysql_server" "example" {
   administrator_login          = "mysqladmin"
   administrator_login_password = "password123"
 
-  # Configure the server to use the example-subnet
-  subnet_id = azurerm_subnet.subnet3.name
-  public_network_access_enabled = false
-
-}
-
-resource "azurerm_mysql_database" "example" {
-  name                = "example-mysql-database"
-  server_name         = azurerm_mysql_server.example.id
-  resource_group_name = azurerm_resource_group.az104-06.name
-  charset             = "utf8"
-  collation           = "utf8_general_ci"
+  # Associate the server with the example-subnet
+  vnet_subnet_id = azurerm_virtual_network.example.subnet.id
 }
