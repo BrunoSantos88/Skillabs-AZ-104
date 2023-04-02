e448153fa1009b15c6c1730b800c140ead109d1e
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 14:20:28 -0300

    hubspok

--- LAB06-HubSpock/myql.tf
+++ LAB06-HubSpock/myql.tf
@@ -1,15 +1,3 @@
-resource "azurerm_virtual_network" "example" {
-  name                = "example-vnet"
-  location            = "East US"
-  resource_group_name =  azurerm_resource_group.az104-06.name
-  address_space       = ["10.0.0.0/16"]
-
-  subnet {
-    name           = "example-subnet"
-    address_prefix = "10.0.1.0/24"
-  }
-}
-
 resource "azurerm_mysql_server" "example" {
   name                = "example-mysql-server"
   location            = "East US"
@@ -22,5 +10,16 @@ resource "azurerm_mysql_server" "example" {
   administrator_login_password = "password123"
 
   # Associate the server with the example-subnet
-  vnet_subnet_id = azurerm_virtual_network.example.subnet.id
+  subnet_id = azurerm_subnet.subnet3.id
+
+}
+
+resource "azurerm_mysql_database" "example" {
+  name                = "example-mysql-database"
+  resource_group_name = azurerm_resource_group.az104-06.name
+  server_name         = azurerm_mysql_server.example.name
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+
+  # ...
 }
