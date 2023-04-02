f3231eb95bb569157398ebbc23573cb708a39ddf
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-02 17:05:01 -0300

    hubspok

--- LAB06-HubSpock/server.sql.tf
+++ LAB06-HubSpock/server.sql.tf
@@ -25,8 +25,8 @@ resource "azurerm_mysql_database" "az104mywsql" {
   collation           = "utf8_unicode_ci"
 }
 
-resource "azurerm_mysql_virtual_network_rule" "myqlrules" {
-  name                = "server-mysqlrules"
+resource "azurerm_mysql_virtual_network_rule" "az104-sqlrules" {
+  name                = "mysqlrules"
   server_name         = azurerm_mysql_server.az104myzsql.name
   resource_group_name = azurerm_resource_group.az104-06.name
   subnet_id           = azurerm_subnet.subnet3.id
