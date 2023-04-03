9ed92404264c6d0b7c1623548c52df03dcf0be33
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:33:03 -0300

    lab4

--- LAB04-VirtualNetworking/resoucegroup.tf
+++ LAB04-VirtualNetworking/resoucegroup.tf
@@ -2,8 +2,8 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "az104-04" {
- # name     = "az104-04-rg1"
- # location = "East US"
-#}
+resource "azurerm_resource_group" "az104-04" {
+  name     = "az104-04-rg1"
+  location = "East US"
+}
 
