84299f3301081a39e05f8d287cedd141150e7ca2
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:20:55 -0300

    lab4

--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -5,15 +5,16 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
     network_interface_ids = [azurerm_network_interface.vm01.id]
     size                  = "Standard_DS1_v2"
 
-  os_profile_linux_config {
-  custom_data = base64encode(file("setup.sh"))
-}
-
     os_disk {
         name              = "az104-disk1"
         caching           = "ReadWrite"
         storage_account_type = "Premium_LRS"
     }
+
+  os_profile_linux_config {
+  custom_data = base64encode(file("setup.sh"))
+}
+
 
     source_image_reference {
         publisher = "Canonical"
