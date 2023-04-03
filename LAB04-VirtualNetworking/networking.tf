3f4a57a7b7d77d0005e1d6cbe193989008faa413
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 13:12:03 -0300

    lab4

--- LAB04-VirtualNetworking/networking.tf
+++ LAB04-VirtualNetworking/networking.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "az104-05-vnet1"
+  name                = "az104-04-vnet1"
   address_space       = ["10.40.0.0/20"]
   location            = azurerm_resource_group.az104-04.location
   resource_group_name = azurerm_resource_group.az104-04.name
@@ -8,7 +8,7 @@ resource "azurerm_virtual_network" "vnet1" {
 
 resource "azurerm_subnet" "subnet0" {
   name                 = "subnet0"
-  resource_group_name = azurerm_resource_group.az104-04.name
+  resource_group_name  = azurerm_resource_group.az104-04.name
   virtual_network_name = azurerm_virtual_network.vnet1.id
   address_prefixes     = ["10.40.0.0/24"]
 }
