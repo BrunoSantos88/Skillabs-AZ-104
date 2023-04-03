59d97ea4335c7bc0cfd02110acdb3f01cd287815
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 18:14:13 -0300

    lab4

--- LAB04-VirtualNetworking/dnsrecord.tf
+++ LAB04-VirtualNetworking/dnsrecord.tf
@@ -1,5 +1,5 @@
 # Create the Azure DNS Zone for the domain
 resource "azurerm_dns_zone" "contoso" {
-  name                = "contoso.org.net"
+  name                = "contoso.com"
   resource_group_name = azurerm_resource_group.az104-04.name
 }
