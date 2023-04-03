6129e1f5a44c2d510077c906c3a98e2447ab952b
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:26:37 -0300

    lab4

--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -29,4 +29,35 @@ os_profile {
     }
 }
 
+resource "azurerm_linux_virtual_machine" "az-104-vm0" {
+    name                  = "az104-vm0"
+    location           = azurerm_resource_group.az104-04.location
+    resource_group_name = azurerm_resource_group.az104-04.name
+    network_interface_ids = [azurerm_network_interface.vm00.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "az104-disk0"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+os_profile {
+    computer_name  = "az104-vm0"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+}
+
+    tags = {
+        environment = "VM0"
+    }
+}
+
 
