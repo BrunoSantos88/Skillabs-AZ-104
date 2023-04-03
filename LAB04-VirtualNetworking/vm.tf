dd5bd089bcbba646146ed3c22616172ab8630094
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:02:49 -0300

    lab4

--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -32,13 +32,13 @@ resource "azurerm_linux_virtual_machine" "az-104-vm0" {
     }
 }
 
-resource "azurerm_linux_virtual_machine" "az-104-vm0" {
-    name                  = "az104-vm0"
+resource "azurerm_linux_virtual_machine" "az-104-vm1" {
+    name                  = "az104-vm1"
     location           = azurerm_resource_group.az104-04.location
     resource_group_name = azurerm_resource_group.az104-04.name
     network_interface_ids = [azurerm_network_interface.vm01.id]
     size                  = "Standard_DS1_v2"
-    computer_name  = "az104-vm0"
+    computer_name  = "az104-vm1"
     admin_username = "azureuser"
     disable_password_authentication = true
 
