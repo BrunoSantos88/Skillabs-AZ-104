7f58e2fd950fb012c31173e51075a9ab9e520348
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:02:14 -0300

    lab4

--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -4,10 +4,14 @@ resource "azurerm_linux_virtual_machine" "az-104-vm0" {
     resource_group_name = azurerm_resource_group.az104-04.name
     network_interface_ids = [azurerm_network_interface.vm00.id]
     size                  = "Standard_DS1_v2"
+    computer_name  = "az104-vm0"
+    admin_username = "azureuser"
+    disable_password_authentication = true
 
-    os_profile_linux_config {
-    custom_data         = file("nginx.sh")
 
+    admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
     }
 
     os_disk {
@@ -22,35 +26,30 @@ resource "azurerm_linux_virtual_machine" "az-104-vm0" {
         sku       = "18.04-LTS"
         version   = "latest"
     }
-
-    computer_name  = "az104-vm0"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = file("~/.ssh/id_rsa")
-    }
      
     tags = {
         environment = "VM0"
     }
 }
 
-resource "azurerm_linux_virtual_machine" "az-104-vm1" {
-    name                  = "az104-vm1"
+resource "azurerm_linux_virtual_machine" "az-104-vm0" {
+    name                  = "az104-vm0"
     location           = azurerm_resource_group.az104-04.location
     resource_group_name = azurerm_resource_group.az104-04.name
     network_interface_ids = [azurerm_network_interface.vm01.id]
     size                  = "Standard_DS1_v2"
+    computer_name  = "az104-vm0"
+    admin_username = "azureuser"
+    disable_password_authentication = true
 
-    os_profile_linux_config {
-    custom_data         = file("nginx.sh")
+
+    admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/id_rsa.pub")
     }
 
     os_disk {
-        name              = "az104-disk1"
+        name              = "az104-disk0"
         caching           = "ReadWrite"
         storage_account_type = "Premium_LRS"
     }
@@ -61,16 +60,7 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
         sku       = "18.04-LTS"
         version   = "latest"
     }
-
-    computer_name  = "az104-vm1"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = file("~/.ssh/id_rsa")
-    }
-
+     
     tags = {
         environment = "VM1"
     }
