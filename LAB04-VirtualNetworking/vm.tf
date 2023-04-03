bf217bdf150d581c5ca26c258069ac5a1c44e3ef
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:39:58 -0300

    lab4

--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -1,3 +1,32 @@
+resource "azurerm_linux_virtual_machine" "az-104-vm0" {
+    name                  = "az104-vm0"
+    location           = azurerm_resource_group.az104-04.location
+    resource_group_name = azurerm_resource_group.az104-04.name
+    network_interface_ids = [azurerm_network_interface.vm00.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "az104-disk0"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "az104-vm0"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+
+    tags = {
+        environment = "VM0"
+    }
+}
+
 resource "azurerm_linux_virtual_machine" "az-104-vm1" {
     name                  = "az104-vm1"
     location           = azurerm_resource_group.az104-04.location
@@ -27,33 +56,5 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
     }
 }
 
-resource "azurerm_linux_virtual_machine" "az-104-vm0" {
-    name                  = "az104-vm0"
-    location           = azurerm_resource_group.az104-04.location
-    resource_group_name = azurerm_resource_group.az104-04.name
-    network_interface_ids = [azurerm_network_interface.vm00.id]
-    size                  = "Standard_DS1_v2"
-
-    os_disk {
-        name              = "az104-disk0"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "az104-vm0"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-    tags = {
-        environment = "VM0"
-    }
-}
 
 
