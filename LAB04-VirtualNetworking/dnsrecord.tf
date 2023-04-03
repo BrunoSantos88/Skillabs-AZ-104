32f1797d5de211089dda3dd825b0ded01cb8ef1f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 18:16:54 -0300

    lab4

--- LAB04-VirtualNetworking/dnsrecord.tf
+++ LAB04-VirtualNetworking/dnsrecord.tf
@@ -3,3 +3,18 @@ resource "azurerm_dns_zone" "contoso" {
   name                = "contoso.com"
   resource_group_name = azurerm_resource_group.az104-04.name
 }
+
+resource "azurerm_dns_a_record" "primary" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.contoso.name
+  records             = ["10.40.1.4"]
+  ttl                 = 300
+}
+
+# Create an A record for the secondary VM
+resource "azurerm_dns_a_record" "secondary" {
+  name                = "www"
+  zone_name           = azurerm_dns_zone.contoso.name
+  records             = ["10.40.0.4"]
+  ttl                 = 300
+}
