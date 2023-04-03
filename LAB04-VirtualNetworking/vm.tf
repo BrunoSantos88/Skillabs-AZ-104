56088915b80108bc79de7b89d3f5225a92ae81c8
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-03 17:10:41 -0300

    lab4

--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -1,50 +1,12 @@
-resource "azurerm_linux_virtual_machine" "az-104-vm0" {
-    name                  = "az104-vm0"
-    location           = azurerm_resource_group.az104-04.location
-    resource_group_name = azurerm_resource_group.az104-04.name
-    network_interface_ids = [azurerm_network_interface.vm00.id]
-    size                  = "Standard_DS1_v2"
-    
-
-    os_disk {
-        name              = "az104-disk0"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    os_profile {
-    computer_name  = "az104-vm0"
-    admin_username = "azureuser"
-    custom_data    = file("azure-user-data.sh")
-  }
-  os_profile_linux_config {
-    disable_password_authentication = false
-  }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
-     
-    tags = {
-        environment = "VM0"
-    }
-}
-
 resource "azurerm_linux_virtual_machine" "az-104-vm1" {
     name                  = "az104-vm1"
     location           = azurerm_resource_group.az104-04.location
     resource_group_name = azurerm_resource_group.az104-04.name
     network_interface_ids = [azurerm_network_interface.vm01.id]
     size                  = "Standard_DS1_v2"
-    computer_name  = "az104-vm1"
-    admin_username = "azureuser"
-    disable_password_authentication = true
 
     os_disk {
-        name              = "az104-disk0"
+        name              = "az104-disk1"
         caching           = "ReadWrite"
         storage_account_type = "Premium_LRS"
     }
@@ -55,9 +17,16 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
         sku       = "18.04-LTS"
         version   = "latest"
     }
-     
+
+    computer_name  = "az104-vm1"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+    custom_data    = file("azure-user-data.sh")
+
+
     tags = {
         environment = "VM1"
     }
 }
+
 
