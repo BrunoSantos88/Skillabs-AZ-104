72e2f7568b29fea7e51842f0a1eba75dd610bd12
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-04 21:17:35 -0300

    labhubspock

--- LAB06-HubSpock/ippublico.tf
+++ LAB06-HubSpock/ippublico.tf
@@ -1,5 +1,16 @@
-resource "azurerm_public_ip" "example" {
-  name                = "example-publicip"
+resource "azurerm_public_ip" "frontend" {
+  name                = "frontend-publicip"
+  location            = azurerm_resource_group.az104-rs5.location
+  resource_group_name = azurerm_resource_group.az104-rs5.name
+  allocation_method   = "Dynamic"
+
+  tags = {
+    environment = "FRONTEND"
+  }
+}
+
+resource "azurerm_public_ip" "backend" {
+  name                = "backend-publicip"
   location            = azurerm_resource_group.az104-rs5.location
   resource_group_name = azurerm_resource_group.az104-rs5.name
   allocation_method   = "Dynamic"
