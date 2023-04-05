5e410ad3d42a6e9a3aa572095f0edcb146450e32
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-04 21:14:23 -0300

    labhubspock

--- LAB06-HubSpock/ippublico.tf
+++ LAB06-HubSpock/ippublico.tf
@@ -1,11 +1,11 @@
 resource "azurerm_public_ip" "example" {
   name                = "example-publicip"
-  location            = "East US"
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.az104-06.location
+  resource_group_name = azurerm_resource_group.az104-06.name
   allocation_method   = "Dynamic"
 
   tags = {
-    environment = "dev"
+    environment = "FRONTEND"
   }
 
   sku {
