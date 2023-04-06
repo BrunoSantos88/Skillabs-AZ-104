9190e7c934b87285406871663bb6eeef8d1f2ccd
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-06 18:13:10 -0300

    publickey

--- VirtualNetworking/dnsrecord.tf
+++ VirtualNetworking/dnsrecord.tf
@@ -6,18 +6,17 @@ resource "azurerm_dns_zone" "contoso" {
 
 #para funcionar parametro do IP tem que esta setado.
 
-resource "azurerm_dns_a_record" "primary" {
-  name                = "www"
-  resource_group_name = azurerm_resource_group.az104-04.name
-  zone_name           = azurerm_dns_zone.contoso.name
-  records             = [azurerm_public_ip.mypublicip1.id] # IP Publico alterar
-  ttl                 = 300
-}
+#resource "azurerm_dns_a_record" "primary" {
+ # name                = "www"
+ # resource_group_name = azurerm_resource_group.az104-04.name
+ #zone_name           = azurerm_dns_zone.contoso.name
+  #records             = [azurerm_public_ip.mypublicip1.id] # IP Publico alterar
+ # ttl                 = 300
+#}
 
-resource "azurerm_dns_a_record" "secondary" {
-  name                = "www"
-  resource_group_name = azurerm_resource_group.az104-04.name
-  zone_name           = azurerm_dns_zone.contoso.name
-  records             = [azurerm_public_ip.mypublicip0.id] #IP Publico alterar
-  ttl                 = 300
-}
+#resource "azurerm_dns_a_record" "secondary" {
+ ## resource_group_name = azurerm_resource_group.az104-04.name
+  #zone_name           = azurerm_dns_zone.contoso.name
+ # records             = [azurerm_public_ip.mypublicip0.id] #IP Publico alterar
+ # ttl                 = 300
+#}
