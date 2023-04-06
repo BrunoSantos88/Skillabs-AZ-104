9daa5c4d81d482a6e53abcad0bb8489e9b68b06a
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-05 21:50:46 -0300

    labaz104

--- LAB04-VirtualNetworking/dnsrecord.tf
+++ LAB04-VirtualNetworking/dnsrecord.tf
@@ -1,6 +1,6 @@
 # Create the Azure DNS Zone for the domain
 resource "azurerm_dns_zone" "contoso" {
-  name                = "siteweb.contoso.com"
+  name                = "contoso.com"
   resource_group_name = azurerm_resource_group.az104-04.name
 }
 
@@ -8,7 +8,7 @@ resource "azurerm_dns_a_record" "primary" {
   name                = "www"
   resource_group_name = azurerm_resource_group.az104-04.name
   zone_name           = azurerm_dns_zone.contoso.name
-  records             = ["20.51.163.99"]
+  records             = ["IP"]
   ttl                 = 300
 }
 
@@ -17,6 +17,6 @@ resource "azurerm_dns_a_record" "secondary" {
   name                = "www"
   resource_group_name = azurerm_resource_group.az104-04.name
   zone_name           = azurerm_dns_zone.contoso.name
-  records             = ["74.235.3.15"]
+  records             = ["IP"]
   ttl                 = 300
 }
--- LAB04-VirtualNetworking/vm.tf
+++ LAB04-VirtualNetworking/vm.tf
@@ -24,7 +24,7 @@ resource "azurerm_linux_virtual_machine" "az-104-vm0" {
 
     admin_ssh_key {
         username       = "azureuser"
-        public_key     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHiBXSqnB0ApgyG2M2n6SxJd9alb8MTKVI/dd834eQV/rdMlw2nXEvoreMqBQFc1K09xbzWWldDLBAROHhRxbO4hACt8X1OPRkswyLHH+im6ee9/k3CIxvOO/PqQ9JLDx/ig4WgLud8QnxT8nu893yl3lJMO+XhXh+7pbaL69zsLi1ZfxGo6pbGhZV6xTzILk/WParqkzDc3/s5FsGYej7oaacs65zm2hugatbASJJpwOBypcIUpyehdACVu8VAHWMIaGYIuplFS1v2a3Vg0INCyJS/X8BG768ov7Q43x/tqUM3weaRlilPWAL3u3XZsRgRXOmOfUvZQoKO1ZJfLLuG/HpUzDMWKPcp1YqV3JChbZVQACx/AeJxGyUeGepv2EbvgIwu2B3ONntW7qXVK+ck1Kx8ZQUhotM/FcHSNPmxrToQKYA9Ytu4CsD5pFPlOAIaYzXgRbfg7ZZNuuKir/teUB+4fZqH9p6KaYg1fkGLfE5F+u4k+rmTEd90XQ5qc7N7LwCcAGJtg1Zd+QBVqcXTyJmyUDJMbgwMI/RZIO46cNJbwiNE2F+JJvAEz+InyQa7RR+z684jq8P54RbszGmMotzprECvc1/aLC1zB6Gf/JrjFotyWo/qLAL0Ue2u+1IO91F+3iWgNaLZ43KPe/djcjAqAteoZzScRK+jIxlCw== brunosantosc1@gmail.com"
+        public_key     = "SuaChavePublica" ##publickey
     }
 
 
@@ -59,7 +59,7 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
 
     admin_ssh_key {
         username       = "azureuser"
-        public_key     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDHiBXSqnB0ApgyG2M2n6SxJd9alb8MTKVI/dd834eQV/rdMlw2nXEvoreMqBQFc1K09xbzWWldDLBAROHhRxbO4hACt8X1OPRkswyLHH+im6ee9/k3CIxvOO/PqQ9JLDx/ig4WgLud8QnxT8nu893yl3lJMO+XhXh+7pbaL69zsLi1ZfxGo6pbGhZV6xTzILk/WParqkzDc3/s5FsGYej7oaacs65zm2hugatbASJJpwOBypcIUpyehdACVu8VAHWMIaGYIuplFS1v2a3Vg0INCyJS/X8BG768ov7Q43x/tqUM3weaRlilPWAL3u3XZsRgRXOmOfUvZQoKO1ZJfLLuG/HpUzDMWKPcp1YqV3JChbZVQACx/AeJxGyUeGepv2EbvgIwu2B3ONntW7qXVK+ck1Kx8ZQUhotM/FcHSNPmxrToQKYA9Ytu4CsD5pFPlOAIaYzXgRbfg7ZZNuuKir/teUB+4fZqH9p6KaYg1fkGLfE5F+u4k+rmTEd90XQ5qc7N7LwCcAGJtg1Zd+QBVqcXTyJmyUDJMbgwMI/RZIO46cNJbwiNE2F+JJvAEz+InyQa7RR+z684jq8P54RbszGmMotzprECvc1/aLC1zB6Gf/JrjFotyWo/qLAL0Ue2u+1IO91F+3iWgNaLZ43KPe/djcjAqAteoZzScRK+jIxlCw== brunosantosc1@gmail.com"
+        public_key     = "*"#publickey
     }
 
 
