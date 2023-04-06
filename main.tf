c2ba196762b6a45815c38082eb60b82c1beae69c
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-05 21:57:18 -0300

    labaz104

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 # SELECIONAR MODULO ANTES DE INICIAR TERRAFORM
 
 #module "VIRTUAL_MACHINE_DNS" {
- # source = "./LAB04-VirtualNetworking"   
+ # source = "./VirtualNetworking"   
 #}
 
 #module "LAB-PERING_CONECTION" {
-#   source = "./LAB05-PeringConection"   
+#   source = "./PeringConection"   
 #}
 
 module "LAB-HUBSPOCK" {
-  source = "./LAB06-HubSpock"
+  source = "./HubSpock"
 }
