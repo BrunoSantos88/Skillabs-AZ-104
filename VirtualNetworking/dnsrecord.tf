215aa31492be2df90eae5c2e49c356ca0ed5bf88
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-06 18:26:18 -0300

    publickey

--- VirtualNetworking/dnsrecord.tf
+++ VirtualNetworking/dnsrecord.tf
@@ -1,22 +1,20 @@
 # Create the Azure DNS Zone for the domain
 resource "azurerm_dns_zone" "contoso" {
-  name                = "contoso.com"
+  name                = "web.contoso.com"
   resource_group_name = azurerm_resource_group.az104-04.name
 }
 
-#para funcionar parametro do IP tem que esta setado.
+resource "azurerm_dns_a_record" "primary" {
+  name                = "www"
+  resource_group_name = azurerm_resource_group.az104-04.name
+ zone_name           = azurerm_dns_zone.contoso.name
+  records             = [azurerm_public_ip.mypublicip1.id] # IP Publico alterar
+  ttl                 = 300
+}
 
-#resource "azurerm_dns_a_record" "primary" {
- # name                = "www"
- # resource_group_name = azurerm_resource_group.az104-04.name
- #zone_name           = azurerm_dns_zone.contoso.name
-  #records             = [azurerm_public_ip.mypublicip1.id] # IP Publico alterar
- # ttl                 = 300
-#}
-
-#resource "azurerm_dns_a_record" "secondary" {
- ## resource_group_name = azurerm_resource_group.az104-04.name
-  #zone_name           = azurerm_dns_zone.contoso.name
- # records             = [azurerm_public_ip.mypublicip0.id] #IP Publico alterar
- # ttl                 = 300
-#}
+resource "azurerm_dns_a_record" "secondary" {
+ resource_group_name = azurerm_resource_group.az104-04.name
+  zone_name           = azurerm_dns_zone.contoso.name
+  records             = [azurerm_public_ip.mypublicip0.id] #IP Publico alterar
+  ttl                 = 300
+}
