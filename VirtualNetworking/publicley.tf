909c2abcaa53d7cbabcccc66bc1e143495d89d06
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-04-10 12:31:10 -0300

    az-104

--- VirtualNetworking/publicley.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_ssh_public_key" "example" {
-  name                = "example"
-  location           = azurerm_resource_group.az104-04.location
-  resource_group_name = azurerm_resource_group.az104-04.name
-  public_key          = file("~/.ssh/id_rsa.pub")
-}
