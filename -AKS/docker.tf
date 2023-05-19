1081c6f24e138c1140a902470bff75a52894efc6
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-05-19 14:24:56 -0300

    az-104

--- -AKS/docker.tf
+++ -AKS/docker.tf
@@ -4,9 +4,4 @@ resource "azurerm_container_registry" "acr" {
   location            = var.location
   sku                 = "Premium"
   admin_enabled       = false
-  georeplications {
-    location                = "East US"
-    zone_redundancy_enabled = true
-    tags                    = {}
-  }
 }
