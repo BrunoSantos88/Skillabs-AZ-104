b92a6549cb082aadc89d77c7f1408ee8b7b2ef4b
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-05-19 14:20:56 -0300

    az-104

---
+++ -AKS/docker.tf
@@ -0,0 +1,17 @@
+resource "azurerm_container_registry" "acr" {
+  name                = "aplication"
+  resource_group_name = var.resoucegroup
+  location            = var.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
