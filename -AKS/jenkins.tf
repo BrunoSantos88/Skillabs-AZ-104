97adcc7207fbc323c09e50fb001e2f9d856a9a5a
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-05-19 16:58:02 -0300

    az-104

--- -AKS/jenkins.tf
+++ -AKS/jenkins.tf
@@ -14,7 +14,7 @@ resource "azurerm_linux_virtual_machine" "jenkins-vm" {
     source_image_reference {
         publisher = "Canonical"
         offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
+        sku       = "20_04-lts"
         version   = "latest"
     }
 
