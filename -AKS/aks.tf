3b7e188fbce452d43adc2065e4da1588f11d823f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-05-19 13:15:25 -0300

    az-104

--- -AKS/aks.tf
+++ -AKS/aks.tf
@@ -1,12 +1,12 @@
 resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
+  name                = "aks1"
   location            = var.location
   resource_group_name = var.resoucegroup
   dns_prefix          = "exampleaks1"
 
   default_node_pool {
-    name       = "default"
-    node_count = 3
+    name       = "aks"
+    node_count = 2
     vm_size    = "Standard_D2_v2"
   }
 
