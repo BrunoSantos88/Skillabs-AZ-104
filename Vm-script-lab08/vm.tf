cf3c57b5c9b3b1912c142f1aa9a66fe6f3fd3e66
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-06-28 14:57:30 -0300

    labs104

--- Vm-script-lab08/vm.tf
+++ Vm-script-lab08/vm.tf
@@ -24,6 +24,7 @@ resource "azurerm_linux_virtual_machine" "az-104-vm0" {
 
     admin_ssh_key {
         username       = "azureuser"
+        public_key     = azurerm_ssh_public_key.chave_ssh.id
     }
 
 
@@ -58,6 +59,7 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
 
     admin_ssh_key {
         username       = "azureuser"
+        public_key     = azurerm_ssh_public_key.chave_ssh.id
     }
 
 
