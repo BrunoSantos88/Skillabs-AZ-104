1a96f4837011fdd256dba61b69ebf9a53e2d12d7
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-06-28 14:27:50 -0300

    lab8

---
+++ Vm-script-lab08/README.md
@@ -0,0 +1,21 @@
+# Skillabs-AZ-104-Terraform
+
+</p>
+<img src="https://user-images.githubusercontent.com/91704169/235208359-7319be57-0a36-46e7-be52-580f9a6f99df.png" min-width=1000px" max-width="100px" width="1000px" align="centter" alt="Lab08">
+
+Laboratorios Skillabs AZ-104 -- Guia para uso do Terraform para criar metodologia rapida para agilizar criação dos LABS
+
+Exercício 1
+Tarefa 1: provisionar o ambiente de laboratório
+Nesta tarefa, você implantará uma máquina virtual do Azure que usará posteriormente neste laboratório.
+
+Neste laboratório, você irá:
+
+Tarefa 1: implantar máquinas virtuais do Azure com resiliência de zona usando o portal do Azure e um modelo do Azure Resource Manager
+Tarefa 2: Configurar máquinas virtuais do Azure usando extensões de máquina virtual
+Tarefa 3: dimensionar computação e armazenamento para máquinas virtuais do Azure
+Tarefa 4: registrar os provedores de recursos Microsoft.Insights e Microsoft.AlertsManagement
+Tarefa 5: implantar conjuntos de dimensionamento de máquinas virtuais do Azure resilientes à zona usando o portal do Azure
+Tarefa 6: Configurar conjuntos de dimensionamento de máquinas virtuais do Azure usando extensões de máquinas virtuais
+Tarefa 7: dimensionar computação e armazenamento para conjuntos de dimensionamento de máquinas virtuais do Azure (opcional)
+
---
+++ Vm-script-lab08/vm.tf
@@ -0,0 +1,69 @@
+resource "azurerm_linux_virtual_machine" "az-104-vm0" {
+    name                  = "az104-vm0"
+    location           = azurerm_resource_group.az104-08.location
+    resource_group_name = azurerm_resource_group.az104-08.name
+    network_interface_ids = [azurerm_network_interface.vm00.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "az104-disk0"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "az104-vm0"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username       = "azureuser"
+        public_key     = azurerm_ssh_public_key.chave_ssh.id
+    }
+
+
+    tags = {
+        environment = "VM0"
+    }
+}
+
+resource "azurerm_linux_virtual_machine" "az-104-vm1" {
+    name                  = "az104-vm1"
+    location           = azurerm_resource_group.az104-08.location
+    resource_group_name = azurerm_resource_group.az104-08.name
+    network_interface_ids = [azurerm_network_interface.vm01.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "az104-disk1"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "az104-vm1"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+
+    admin_ssh_key {
+        username       = "azureuser"
+        public_key     = azurerm_ssh_public_key.chave_ssh.id
+    }
+
+
+    tags = {
+        environment = "VM1"
+    }
+}
