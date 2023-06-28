6d8c47f42873245b418bb4ae904e83586eb08909
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-06-28 15:10:57 -0300

    labs104

--- Vm-script-lab08/vm.tf
+++ Vm-script-lab08/vm.tf
@@ -1,3 +1,4 @@
+#VM0
 resource "azurerm_linux_virtual_machine" "az-104-vm0" {
     name                  = "az104-vm0"
     location           = azurerm_resource_group.az104-08.location
@@ -29,6 +30,41 @@ resource "azurerm_linux_virtual_machine" "az-104-vm0" {
 
     tags = {
         environment = "VM0"
+    }
+}
+
+#vm1
+resource "azurerm_linux_virtual_machine" "az-104-vm1" {
+    name                  = "az104-vm0"
+    location           = azurerm_resource_group.az104-08.location
+    resource_group_name = azurerm_resource_group.az104-08.name
+    network_interface_ids = [azurerm_network_interface.vm00.id]
+    size                  = "Standard_DS1_v2"
+
+    os_disk {
+        name              = "az104-disk0"
+        caching           = "ReadWrite"
+        storage_account_type = "Premium_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "az104-vm1"
+    admin_username = "azureuser"
+    disable_password_authentication = true
+
+   admin_ssh_key {
+    username   = "azureuser"
+    public_key = tls_private_key.example_ssh.public_key_openssh
+  }
+
+    tags = {
+        environment = "VM1"
     }
 }
 
