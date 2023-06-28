c3a632f851241c5c061741d4f61346eaefa80f76
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-06-28 15:12:19 -0300

    labs104

--- Vm-script-lab08/vm.tf
+++ Vm-script-lab08/vm.tf
@@ -38,7 +38,7 @@ resource "azurerm_linux_virtual_machine" "az-104-vm1" {
     name                  = "az104-vm0"
     location           = azurerm_resource_group.az104-08.location
     resource_group_name = azurerm_resource_group.az104-08.name
-    network_interface_ids = [azurerm_network_interface.vm00.id]
+    network_interface_ids = [azurerm_network_interface.vm01.id]
     size                  = "Standard_DS1_v2"
 
     os_disk {
